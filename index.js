f49ca8274d69a78d3075321e0fc8902c8ec31a75
Liana-Geo <l.geonjyan@hbk-bs.de>
2025-09-13 13:20:08 +0200

    help
    ok,

--- index.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".project-btn").forEach(btn => {
-  btn.addEventListener("click", () => {
-    const link = btn.getAttribute("data-link");
-    window.open(link, "_blank");
-  });
-});
